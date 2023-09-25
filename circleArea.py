31d5cc7d0b468d2ca97a475bd4d2585e687a8d16
AtulSharma223 <145961662+AtulSharma223@users.noreply.github.com>
2023-09-25 12:18:20 +0530

    circleArea.py
    Circle area and paramter

--- circleArea.py
+++ circleArea.py
@@ -1,12 +1,15 @@
+import math
 class circle:
-  def area(r):
-    area=math.pi*r*r
+  def __init__(self,r):
+    self.r=r
+  def area(self):
+    area=math.pi*(self.r**2)
     return area
-  def paremeter(r):
-    p=2*math.pi.r
+  def paremeter(self):
+    p=2*math.pi*(self.r)
     return p
 
 radius=float(input("Enter radius "))
-circle1=cirlce()
-print("Arear is",circle1.area(radius))
-print("Paremeter is",circle1.paremeter(radius))
+circle1=circle(radius)
+print("Arear is",circle1.area())
+print("Paremeter is",circle1.paremeter())
